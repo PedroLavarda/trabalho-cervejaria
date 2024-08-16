bc6014f5ec3e9f1e265fc59966946ca45250d978
PedroLavarda <plzorel@gmail.com>
2024-08-16 15:46:13 -0300

    start refactor

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 import { navbarComponent } from "./components/navbar.js";
 import { footerComponent } from "./components/footer.js";
 
-
 document.getElementById("navbar").innerHTML = navbarComponent;
 document.getElementById("footer").innerHTML = footerComponent;
--- js/cadastro.js
+++ js/cadastro.js
@@ -3,16 +3,16 @@ import { User } from "./userModel.js";
 const cadastroForm = document.getElementById("cadastroForm");
 const cadastroButton = document.getElementById("cadastroFormSubmit");
 
-const users = [];
-
 cadastroButton.addEventListener("click", (e) => {
     e.preventDefault();
     const email = cadastroForm.inputEmail.value;
     const password = cadastroForm.inputPassword.value;
 
-    const newUser = new User(email, password);
+    const user = new User(email, password);
 
-    users.push(newUser);
+    const users = JSON.parse(localStorage.getItem("users")) || []
+
+    users.push(user);
 
     localStorage.setItem("users", JSON.stringify(users));
     window.location.href = "login.html";
