f23fe92023276fb59d43e51c80c73547a8f755c6
FelipeVilhalva <felipervilhalva@gmail.com>
2024-08-31 18:16:36 -0300

    Navbar

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,12 +1,12 @@
 export const navbarComponent = 
     `
-        <img class="img-fluid rounded img-logo ms-5 mt-2" src="assets/images/logo-cervejaria.png" alt="logo-cervejaria">
+        <img class="img-fluid rounded img-logo ms-5 mt-2" src="assets/images/logo-cervejaria.png" alt="logo-cervejaria" id="logo-cervejaria">
 
         <div class="d-flex flex-column align-items-center justify-content-center">
-            <h1 class="fw-bolder fs-1">Cervejaria Caneco Cheio</h1>
+            <h1 class="fw-bolder" id="titulo-cervejaria">Cervejaria Caneco Cheio</h1>
             <div class="d-flex flex-row mt-3">
-                <button class="btn fw-bold"><a class="text-dark text-decoration-none" href="index.html"> Home </a></button>
-                <button class="btn fw-bold"><a class="text-dark text-decoration-none" href="about.html"> Quem somos </a></button>
+                <button class="btn fw-bold" id="botao-home"><a class="text-dark text-decoration-none" href="index.html"> Home </a></button>
+                <button class="btn fw-bold" id="botao-about"><a class="text-dark text-decoration-none" href="about.html"> Quem somos </a></button>
                 <button id="navbarProfileBtn" class="btn fw-bold"> Perfil </button>
             </div>
         </div>
@@ -15,7 +15,7 @@ export const navbarComponent =
             <form class="d-flex">
                 <div class="input-group">
                     <span class="input-group-text bg-white">
-                        <i class="bi bi-search"></i>
+                        <i class="bi bi-search" id="pesquisar-icon"></i>
                     </span>
                     <input class="form-control border rounded me-2" type="search" placeholder="Pesquisar" id="pesquisar">
                 </div>
