52e34f6a6cd463bbe93f6954d9b74fc8c2fb2682
FelipeVilhalva <felipervilhalva@gmail.com>
2024-09-01 00:38:37 -0300

    NAVBAR

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -4,7 +4,7 @@ export const navbarComponent =
 
         <div class="d-flex flex-column align-items-center justify-content-center">
             <h1 class="fw-bolder" id="titulo-cervejaria">Cervejaria Caneco Cheio</h1>
-            <div class="d-flex flex-row mt-3">
+            <div class="d-flex flex-row mt-3 align-items-center justify-content-center">
                 <button class="btn fw-bold" id="botao-home"><a class="text-dark text-decoration-none" href="index.html"> Home </a></button>
                 <button class="btn fw-bold" id="botao-about"><a class="text-dark text-decoration-none" href="about.html"> Quem somos </a></button>
                 <button id="navbarProfileBtn" class="btn fw-bold"> Perfil </button>
@@ -13,7 +13,7 @@ export const navbarComponent =
 
         <div class="d-flex flex-row me-5">
             <form class="d-flex">
-                <div class="input-group">
+                <div id="divPesquisar" class="input-group">
                     <span class="input-group-text bg-white">
                         <i class="bi bi-search" id="pesquisar-icon"></i>
                     </span>
@@ -21,7 +21,7 @@ export const navbarComponent =
                 </div>
             </form>
 
-            <button id="navbarCartBtn" class="ms-2 btn btn-dark text-white w-50"> Carrinho </button>
-            <button id="navbarLogout" class="ms-2 btn btn-danger text-white"><i class="bi bi-box-arrow-left h5"></i></button>
+            <button id="navbarCartBtn" class="ms-2 btn btn-dark text-white"><i id="icon-cart" class="bi bi-cart3 h5"></i></button>
+            <button id="navbarLogout" class="ms-2 btn btn-danger text-white"><i id="icon-logout" class="bi bi-box-arrow-left h5"></i></button>
         </div>
     `
